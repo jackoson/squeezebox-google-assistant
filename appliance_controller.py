946d0b37a7695430808ccd1b27307cd7315a4b55
Samuel Russell <sr14978@bristol.ac.uk>
2018-08-24 01:54:08 +0100

    all lights

--- appliance_controller.py
+++ appliance_controller.py
@@ -2,11 +2,23 @@
 import socket
 
 appliance_codes = {
-  "LANDING LIGHTS"                      : 44,
-  "HALL LIGHTS"                         : 40,
+  "DOWNSTAIRS BATHROOM MAIN LIGHTS"     : 8,
+  "CUPBOARD LIGHTS"                            : 26,
+  "LOUNGE LIGHTS"                              : 29,
   "KITCHEN MAIN LIGHTS"                 : 34,
   "KITCHEN CUPBOARD LIGHTS"             : 37,
-  "UPSTAIRS BATHROOM MAIN LIGHTS"       : 58
+  "HALL LIGHTS"                         : 40,
+  "LANDING LIGHTS"                      : 44,
+  "BEDROOM LAMP"                        : 48,
+  "UTILITY ROOM LIGHTS"                        : 51,
+  "UPSTAIRS BATHROOM MAIN LIGHTS"       : 58,
+  "UPSTAIRS BATHROOM MIRROR LIGHTS"     : 61,
+  "TOILET LIGHTS"                              : 87,
+  "DOWNSTAIRS BATHROOM MIRROR LIGHTS"   : 145,
+  
+  "ALL LIGHTS"                          : 5,
+  "FRONT LIGHTS"                        : 6,
+  "BACK LIGHTS"                         : 7,
 }
 
 def sendApplianceCommand(details): 
@@ -19,20 +31,28 @@ def sendApplianceCommand(details):
     raise Exception("appliance not supported. Must be one of: " + ",".join(appliance_codes.keys()))
 
   appliance_code = appliance_codes[details["appliance"]]
-
-  if details['state'] == "ON":
-    on(appliance_code)
-  elif details["state"] == "OFF":
-    off(appliance_code)
-  else:
-    raise Exception("state not supported. Must be either \"ON\" or \"OFF\".")
-
-def on(code):
-  send_command(b"action flag set 56; flag clear 57; flag set 58; macro run " + bytes(str(code), encoding="ascii") + b"; __wait 100")
- 
-def off(code):
-  send_command(b"action flag clear 56; flag set 57; flag set 58; macro run " + bytes(str(code), encoding="ascii") + b"; __wait 100")
   
+  if details["appliance"] in ["ALL LIGHTS", "FRONT LIGHTS", "BACK LIGHTS"]:
+    run_macro(appliance_code)
+  else:
+  
+    if details['state'] == "ON":
+      switch_on(appliance_code)
+    elif details["state"] == "OFF":
+      switch_off(appliance_code)
+    else:
+      raise Exception("state not supported. Must be either \"ON\" or \"OFF\".")
+
+def switch_on(code):
+  send_command(b"action flag set 56; flag clear 57; flag set 58; \
+    macro run " + bytes(str(code), encoding="ascii") + b"; __wait 100")
+ 
+def switch_off(code):
+  send_command(b"action flag clear 56; flag set 57; flag set 58; \
+  macro run " + bytes(str(code), encoding="ascii") + b"; __wait 100")
+  
+def run_macro(code):
+  send_command(b'action macro run ' + bytes(str(code), encoding="ascii") + '; __wait 100')
 
 def send_command(command):
   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
