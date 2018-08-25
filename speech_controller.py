ed9eaf7f03c71cd2a56c8c34ece91de27fcbaa15
Samuel Russell <sr14978@bristol.ac.uk>
2018-08-25 01:53:03 +0100

    speech tool test

--- speech_controller.py
+++ speech_controller.py
@@ -1,5 +1,12 @@
 
-from subprocess import call
+from subprocess import call,DEVNULL
+from sys import argv
 
 def speak(text):
-  call(['./speech.sh', text])
+  call(['./speech.sh', text], stdout=DEVNULL, stderr=DEVNULL)
+  
+if __name__ == "__main__":
+  if len(argv) > 1:
+    speak(argv[1])
+  else:
+    speak("Stop that please")
