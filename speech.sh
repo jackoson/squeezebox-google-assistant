ff3bcd4667bde022fe57527f5fca05263bdb1872
jackoson <jackosonmail-github@yahoo.co.uk>
2018-08-25 02:21:16 +0100

    reduced speech volume

--- speech.sh
+++ speech.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 say() { 
-  mplayer -ao alsa -really-quiet -noconsolecontrols \
+  mplayer -ao alsa -really-quiet -noconsolecontrols -softvol -volume 20\
   "http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=$*&tl=en";
 }
 say $*
