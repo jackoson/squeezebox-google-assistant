1fbbea776041492e43ebce369ebed0c4341bb4d8
jackoson <jackosonmail-github@yahoo.co.uk>
2018-08-31 15:33:03 +0100

    encoded text for url

--- speech_controller.py
+++ speech_controller.py
@@ -2,11 +2,13 @@
 from subprocess import call,DEVNULL
 from sys import argv
 import os
+from urllib.parse import quote_plus as encode
+
 
 speech_path = os.path.dirname(os.path.realpath(__file__)) + '/speech.sh'
 
 def speak(text):
-  call([speech_path, text], stdout=DEVNULL, stderr=DEVNULL)
+  call([speech_path, encode(text)], stdout=DEVNULL, stderr=DEVNULL)
   
 if __name__ == "__main__":
   if len(argv) > 1:
