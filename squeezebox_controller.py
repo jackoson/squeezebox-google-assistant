9da5e4352601ae149eb4d8680ddfd82ba6124b11
Samuel Russell <sr14978@bristol.ac.uk>
2018-09-01 04:07:49 +0100

    repeat annd shuffle

--- squeezebox_controller.py
+++ squeezebox_controller.py
@@ -16,7 +16,13 @@ commands = {
   "SLEEP SONG": ["jiveendoftracksleep"],
   "SKIP": ["playlist","index","+1"],
   "PREVIOUS": ["playlist","index","-1"],
-  "UNSYNC": ["sync","-"]
+  "UNSYNC": ["sync","-"],
+  "SHUFFLE OFF": ["playlist","shuffle",0],
+  "SHUFFLE SONG": ["playlist","shuffle",1],
+  "SHUFFLE ALBUM": ["playlist","shuffle",2],
+  "REPEAT OFF": ["playlist","repeat",0],
+  "REPEAT SONG": ["playlist","repeat",1],
+  "REPEAT PLAYLIST": ["playlist","repeat",2]
 }
 
 player_macs = {}
